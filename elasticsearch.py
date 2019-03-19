098850bd9308c21d8d0d87dd104f7d225d12f8a6
Shah <khansss1915@gmail.com>
2019-03-19 21:06:24 +0500

    Add Asyncio support to class

--- elasticsearch.py
+++ elasticsearch.py
@@ -9,98 +9,65 @@ import asyncio
 from aioes import Elasticsearch
 from aiohttp import web
 
-
-class AioESService:
-    """
-    Adaptor class to elasticsearch database using aioes Library.
-    It encapsulate connection to elasticsearh database.
-    """
-    def __init__(self, loop=None):
-        self.es = Elasticsearch(["127.0.0.1:9200"], loop=loop)
-    
-    
-    async def get_info(self):
-        """
-        Get elasticsearch database information from cluster
-        """
-        return await self.es.info()
-    
-    
-    async def get(self, index, id,  doc_type='_all'):
-        """
-        Get JSON document from the cluster based on ID
-        """
-        return await self.es.get(index, id, doc_type=doc_type)
-    
-    
-    async def create(self, index, doc_type, body, id=None ):
-        """
-        Add JSON document in a specific index 
-        """
-        return await self.es.create(index, doc_type, body, id=id)
-    
-    
-    async def exists(self, index, id, doc_type='_all' ):
-        """
-        Return boolean indicating whether or not given document exists
-        """
-        return await self.es.exists(index, id, doc_type=doc_type)
-    
-    
-    async def update(self, index, doc_type, id, body = None):
-        """
-        Update document by provided data
-        """
-        return await self.es.update(index, doc_type, id, body=body)
-    
-    
-    async def search(self, index=None, doc_type=None, body=None):
-        """
-        It execute the query and get the results back that matches the query
-        """
-        return await self.es.search(index=index, doc_typ=doc_type, body=body)
-    
-    
-    async def delete(self, index, doc_type, id ):
-        """
-        Delete JSON document from index by ID
-        """
-        return await self.es.delete(index, doc_type, id)
-    
-    
-    async def count(self, index=None, doc_type=None, body=None):
-        """
-        It execute query and return total number of matches for the query
-        """
-        return await self.es.count(index=index, doc_type=doc_type, body=body)
+import os
+import logging
 
 
-class ESBase:
-    """
-    Implementation of elasticsearch adaptor class.
-    this is just how to use elasticsearch with asyncio.
-    """
-    objects = AioESService()
+logger = logging.getLogger("ElasticsearchController")
+
+
+class ElasticsearchController:
+    def __init__(self):
+        self._elasticsearch = None
+        self.elasticsearch_conn = False
+        self._loop = None
+        
+    def get_loop(self):
+        return self._loop
+
+    def get_elasticsearch_client(self):
+        return self._elasticsearch_client
+
+    async def get_value(self, index: str, id: str, doc_type='_all') -> str:
+        try:
+            assert await self._elasticsearch_client.exists(index, id) is True
+            result = await self._elasticsearch_client.get(index, id)
+        except AssertionError as e:
+            logger.error("Elasticsearch client doesn't exist when it should. " + str(e))
+            result = ""
+
+        result = result.decode('utf-8')
+        return result
+
+    def set_loop(self, loop):
+        self._loop = loop
+
+    def add_elasticsearch_client(self):
+        elasticsearch_host = None
+        elasticsearch_port = None
+        try:
+
+            elasticsearch_host = os.getenv("ELASTICSEARCH_HOST", "redis.dev.muchneededllc.com")
+            elasticsearch_port = str(os.getenv("ELASTICSEARCH_PORT", 9200))
             
+        except OSError as e:
+            logger.error("Couldn't get environmental variables for elasticearch. " + str(e))
+            exit(1)
 
+        try:
+            if self._loop is not None:
+                address = ':'.join([elasticsearch_host, elasticsearch_port])
+                self._elasticsearch_client = Elasticsearch([address], loop=self._loop)
+                self.elasticsearch_conn = True
+                logger.debug("Created Elasticsearch Client.")
+            else:
+                logger.error("Couldn't create elasticsearch client because loop hasn't been set.")
 
-async def hello_aioes(request):
-    """
-    Web based interface to elasticsearch implementation.
-    """
-    cluster_info = await ESBase.objects.get_info()
-    second = await ESBase.objects.count()
-    return web.Response(text="{} \n {}".format(cluster_info, second))
+        except Exception as e:
+            logger.error("couldn't open elasticsearch.")
+            raise Exception(e)
 
+    def cleanup(self):
+        if self.elasticsearch_conn:
+            self._elasticsearch_client.close()
 
-def create_app(loop=None):
-    """
-    Web app for the asyncio elasticsearch implementation
-    """
-    app = web.Application(loop=loop)
-    app.router.add_route('GET', '/', hello_aioes)
-    return app
-
-
-if __name__ == "__main__":
-    web.run_app(create_app())
